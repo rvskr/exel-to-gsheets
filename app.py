7b6bc79f33a40022951c7a0a6a128f86498fb579
voskreseniev <roman.mayber@gmail.com>
2024-07-04 13:54:32 +0300

    +

--- app.py
+++ app.py
@@ -2,66 +2,40 @@ import os
 from flask import Flask, request, render_template, jsonify
 import pygsheets
 import pandas as pd
-from openpyxl import load_workbook
-from win32com.client import Dispatch as client
+import xlrd
 import threading
 import pickle
-import pythoncom
 
 app = Flask(__name__)
 
-# Функция для конвертации xls файла в xlsx
-def convert_xls_to_xlsx(xls_file):
+# Функция для загрузки данных из xls файла в DataFrame
+def load_data_from_xls(file_path):
     try:
-        pythoncom.CoInitialize()  # Инициализация COM-библиотек
-        file_path, file_name = os.path.split(xls_file)
-        file_name_without_extension = os.path.splitext(file_name)[0]
-        xlsx_file = os.path.join(file_path, f"{file_name_without_extension}.xlsx")
-        xlsx_file = os.path.normpath(xlsx_file)
-        
-        if os.path.exists(xlsx_file):
-            os.remove(xlsx_file)
-
-        excel = client("Excel.Application")
-        wb = excel.Workbooks.Open(xls_file)
-        wb.SaveAs(xlsx_file, FileFormat=51)
-        wb.Close()
-        excel.Quit()
-        
-        return xlsx_file
+        wb = xlrd.open_workbook(file_path)
+        sheet = wb.sheet_by_index(0)
+        data = []
+        for rownum in range(sheet.nrows):
+            data.append(sheet.row_values(rownum))
+        return pd.DataFrame(data)
     except Exception as e:
-        print(f"Ошибка при конвертации файла: {e}")
+        print(f"Ошибка при загрузке данных из xls файла: {e}")
         return None
-    finally:
-        pythoncom.CoUninitialize()  # Освобождение COM-библиотек
 
 # Функция для загрузки данных в Google Sheets
 def upload_to_google_sheets(gc, selected_sheet, selected_tab, excel_file):
     try:
-        if excel_file.endswith('.xls'):
-            converted_file = convert_xls_to_xlsx(excel_file)
-            if converted_file:
-                excel_file = converted_file
-            else:
-                return {"status": "error", "message": "Ошибка при конвертации файла. Загрузка отменена."}
-        
         sh = gc.open(selected_sheet)
         worksheet = sh.worksheet_by_title(selected_tab)
 
-        if os.path.exists(excel_file):
-            wb = load_workbook(excel_file)
-            ws = wb.active
-            df = pd.DataFrame(ws.values)
+        df = load_data_from_xls(excel_file)
+        if df is not None:
             max_rows, max_cols = min(df.shape[0], 1086), min(df.shape[1], 56)
             df_selected = df.iloc[:max_rows, :max_cols]
             worksheet.clear()
             worksheet.update_values(crange='A1', values=df_selected.values.tolist())
-            
-            if excel_file.endswith('.xlsx'):
-                os.remove(excel_file)
             return {"status": "success", "message": "Данные успешно загружены в Google Sheets."}
         else:
-            return {"status": "error", "message": "Файл Excel не найден."}
+            return {"status": "error", "message": "Ошибка при загрузке данных из xls файла."}
     except Exception as e:
         print(f"Ошибка при загрузке данных: {e}")
         return {"status": "error", "message": f"Ошибка при загрузке данных: {e}"}
